8bc7ebd4a9a473af8329259bc804d5a2b9fcb316
TGRCDev <tgrc@tgrc.dev>
2021-09-12 19:20:46 -0700

    Separating and cleaning code

--- src/world_map/mod.rs
+++ src/world_map/mod.rs
@@ -1,44 +1,8 @@
-use bevy::prelude::*;
-
-use crate::{
-    core::GameState,
-};
-
-#[derive(Default)]
-pub struct WorldMapPlugin;
+mod plugin;
+pub use plugin::*;
 
 #[derive(Debug, Clone, Hash, PartialEq, Eq)]
 pub enum WorldMapState {
     Paused,
     Running
-}
-
-pub struct WorldMapEntity(Entity);
-
-impl Plugin for WorldMapPlugin {
-    fn build(&self, app: &mut AppBuilder) {
-        app.add_state(WorldMapState::Paused)
-            .add_system_set(
-                SystemSet::on_enter(GameState::WorldMap)
-                .with_system(enter_world_map.system())
-            )
-            .add_system_set(
-                SystemSet::on_exit(GameState::WorldMap)
-                .with_system(exit_world_map.system())
-            );
-    }
-}
-
-fn enter_world_map(mut commands: Commands)
-{
-    let map = commands.spawn()
-    .id();
-
-    commands.insert_resource(WorldMapEntity(map));
-}
-
-fn exit_world_map(mut commands: Commands, world_map: Res<WorldMapEntity>)
-{
-    commands.entity(world_map.0).despawn_recursive();
-    commands.remove_resource::<WorldMapEntity>();
 }
